d503d2722692f184f17f22dd9e768c8e13e5e6a8
Aaron Shannon <ayren.shannon@me.com>
2025-06-08 00:27:17 -0700

    completed first Unity script

---
+++ FirstUnityScript/Assets/Scripts/PrintMessage.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Prints a message
+/// </summary>
+public class PrintMessage : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        // Print a message to the console
+        Debug.Log("Hi, n00b!");
+    }
+}
